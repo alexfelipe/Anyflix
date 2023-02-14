a052d4a44a514b01aa9abf4e26725c26ff02913e
Alex Felipe <alexfelipevieira@gmail.com>
2023-02-14 14:28:11 -0300

    projeto inicial

---
+++ app/src/main/java/br/com/alura/anyflix/ui/uistates/MovieDetailsUiState.kt
@@ -0,0 +1,15 @@
+package br.com.alura.anyflix.ui.uistates
+
+import br.com.alura.anyflix.model.Movie
+
+sealed class MovieDetailsUiState {
+
+    object Loading : MovieDetailsUiState()
+
+    object Failure : MovieDetailsUiState()
+
+    data class Success(
+        val movie: Movie,
+        val suggestedMovies: List<Movie> = emptyList()
+    ) : MovieDetailsUiState()
+}
---
+++ app/src/main/java/br/com/alura/anyflix/ui/viewmodels/MovieDetailsViewModel.kt
@@ -0,0 +1,82 @@
+package br.com.alura.anyflix.ui.viewmodels
+
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import br.com.alura.anyflix.database.dao.MovieDao
+import br.com.alura.anyflix.database.entities.toMovie
+import br.com.alura.anyflix.model.Movie
+import br.com.alura.anyflix.navigation.movieIdArgument
+import br.com.alura.anyflix.ui.uistates.MovieDetailsUiState
+import br.com.alura.anyflix.ui.uistates.MovieDetailsUiState.Success
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.flow.flatMapLatest
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+import kotlin.random.Random
+
+@HiltViewModel
+class MovieDetailsViewModel @Inject constructor(
+    private val savedStateHandle: SavedStateHandle,
+    private val dao: MovieDao
+) : ViewModel() {
+    private var currentUiStateJob: Job? = null
+
+    private val _uiState = MutableStateFlow<MovieDetailsUiState>(
+        MovieDetailsUiState.Loading
+    )
+    val uiState = _uiState.asStateFlow()
+
+    init {
+        loadUiState()
+    }
+
+    @OptIn(ExperimentalCoroutinesApi::class)
+    private fun loadUiState() {
+        currentUiStateJob?.cancel()
+        currentUiStateJob = viewModelScope.launch {
+            _uiState.update { MovieDetailsUiState.Loading }
+            delay(Random.nextLong(250, 1000))
+            dao.findMovieById(
+                requireNotNull(
+                    savedStateHandle[movieIdArgument]
+                )
+            ).map { it.toMovie() }
+                .flatMapLatest<Movie, MovieDetailsUiState> { movie ->
+                    dao.suggestedMovies(movie.id)
+                        .map { suggestedMovies ->
+                            Success(
+                                movie = movie,
+                                suggestedMovies = suggestedMovies,
+                            )
+                        }
+                }.catch {
+                    emit(MovieDetailsUiState.Failure)
+                }.collectLatest { uiState ->
+                    _uiState.emit(uiState)
+                }
+        }
+    }
+
+    suspend fun addToMyList(movie: Movie) {
+        dao.addToMyList(movie.id)
+    }
+
+    suspend fun removeFromMyList(movie: Movie) {
+        dao.removeFromMyList(movie.id)
+    }
+
+    fun loadMovie() {
+        loadUiState()
+    }
+
+}
